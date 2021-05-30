a28e5e59c8e190637b30ca4480e444e637da0883
Patrick Reid <patrickreid@Patricks-MBP.home>
2021-05-30 11:24:30 -0400

    fix

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 const {Client} = require('pg')
-var pgp = require('pg-promise')(/*options*/)
 const connectionString = 'postgres://admin1:y2699WmsN8ZfxGDhPpFrUKIfIg9fef2I@oregon-postgres.render.com/vytalize_npi_database?ssl=true';
-var db = pgp('postgres://admin2:y2699WmsN8ZfxGDhPpFrUKIfIg9fef2I@oregon-postgres.render.com/vytalize_npi_database')
 // pools will use environment variables
 // for connection information
-const pool = require('./db')
 app.use(express.json());
 
 const client = new Client({
